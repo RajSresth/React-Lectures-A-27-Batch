cb5ba8a80cb96e95566fe6a775ce1ef14b358f90
Shresth Rajput <rajshresth2@gmail.com>
2025-12-29 16:57:40 +0530

    Bug Fixed

---
+++ Express/NODE/src/api files.md
@@ -0,0 +1,15 @@
+# API LISTS:-
+
+/api
+
+- Authentication
+  auth/signin
+  auth/verifyEmail
+  auth/verifyOTP
+  auth/login
+
+- Protected
+  user/profile
+  user/feed
+  user/cart
+  user/payment
--- Express/NODE/src/app.js
+++ Express/NODE/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.use("/auth/user", userRoutes);
-app.use("/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
 
 export default app;
+
+
